8461421b7c1b5e57c60ec455e15a169cc83049d5
jcalhou9 <jcalhou9@email.cpcc.edu>
2025-04-09 21:41:51 -0400

    update
    update

--- joyofphp/src/CreateDB.php
+++ joyofphp/src/CreateDB.php
@@ -4,7 +4,7 @@
  * Demonstrates how to create a database, create a table, and insert records.
  */
 
- $mysqli = new mysqli($serverName = 'sql212.infinityfree.com', $username = 'if0_38352683', $password = 'Projectsite11');
+ $mysqli = new mysqli($serverName = 'xxxxxxxx', $username = 'xxxxxx', $password = 'xxxxx');
 
 
    if (!$mysqli) { 
